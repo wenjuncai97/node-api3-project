2d3efe88d2bcae34d69e891aeae11011c788a146
Nathan Cai <wenjuncai97@gmail.com>
2022-05-11 21:08:05 -0400

    finished tests

--- api/users/users-router.js
+++ api/users/users-router.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const {
   validateUserId,
-  validateUser
+  validateUser,
+  validatePost
 } = require('../middleware/middleware')
 
 const User = require('./users-model');
@@ -33,32 +34,52 @@ router.post('/', validateUser, (req, res, next) => {
     .catch(next)
 });
 
-router.put('/:id', validateUserId, validateUser, (req, res) => {
+router.put('/:id', validateUserId, validateUser, (req, res, next) => {
   // RETURN THE FRESHLY UPDATED USER OBJECT
   // this needs a middleware to verify user id
   // and another middleware to check that the request body is valid
-  console.log(req.user);
-  console.log(req.name)
+  User.update(req.params.id, {name: req.name})
+    .then(updatedUser => {
+      res.json(updatedUser);
+    })
+    .catch(next)
 });
 
-router.delete('/:id', validateUserId, (req, res) => {
+router.delete('/:id', validateUserId, async (req, res, next) => {
   // RETURN THE FRESHLY DELETED USER OBJECT
   // this needs a middleware to verify user id
-  console.log(req.user);
+  try {
+    const result = await User.remove(req.params.id)
+    res.json(req.user);
+  } catch (err) {
+    next(err)
+  }
 });
 
-router.get('/:id/posts', validateUserId, (req, res) => {
+router.get('/:id/posts', validateUserId, async (req, res, next) => {
   // RETURN THE ARRAY OF USER POSTS
   // this needs a middleware to verify user id
-  console.log(req.user);
+  try {
+    const result = await User.getUserPosts(req.params.id)
+    res.json(result);
+  } catch (err) {
+    next(err)
+  }
 });
 
-router.post('/:id/posts', validateUserId, validateUser, (req, res) => {
+router.post('/:id/posts', validateUserId, validatePost, async (req, res, next) => {
   // RETURN THE NEWLY CREATED USER POST
   // this needs a middleware to verify user id
   // and another middleware to check that the request body is valid
-  console.log(req.user);
-  console.log(req.name)
+  try {
+    const result = await Post.insert({
+      user_id: req.params.id,
+      text: req.text,
+    })
+    res.status(201).json(result)
+  } catch(err) {
+    next(err)
+  }
 });
 
 router.use((err, req, res, next) => {
